cedb4c168e682a0e0503770de75c8c0c29586351
christopher <christopher.hirudayasamy@syncfusion.com>
2020-10-26 17:04:50 +0530

    isssuses

--- gulpfile.js
+++ gulpfile.js
@@ -38,7 +38,7 @@ gulp.task('ship-to-gitlap', function (done) {
         } else {
             console.log('Clone has been completed...!');
             // update src from github to gitlap - replace files from cloed repo
-			shelljs.rm('-rf',`./gitlapRepo/install-docs/node_modules`);
+			shelljs.rm('-rf',`../install-docs/node_modules`);
 			console.log('delete completed...!');
             shelljs.cp('-rf', `../install-docs/*`, `./gitlapRepo/install-docs`);
             shelljs.cd(`./gitlapRepo/install-docs`);

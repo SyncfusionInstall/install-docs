dc40bb8a48ed66a3e74311fbfa76eda5dc365f70
christopher <christopher.hirudayasamy@syncfusion.com>
2020-10-27 09:43:46 +0530

    issues

--- gulpfile.js
+++ gulpfile.js
@@ -39,7 +39,7 @@ gulp.task('ship-to-gitlap', function (done) {
         } else {
             console.log('Clone has been completed...!');
             // update src from github to gitlap - replace files from cloed repo
-			var rootDir = path.resolve('../../gitlapRepo/install-docs');
+			var rootDir = path.resolve('../../../gitlapRepo/install-docs');
 			var rootDir2 = path.resolve('../install-docs');
 			console.log('Directory...!' + rootDir);
 			console.log('Directory...!' + rootDir2);

68d9dd202fc001206b0cbb08a283344b35c63a3d
christopher <christopher.hirudayasamy@syncfusion.com>
2020-10-27 16:46:33 +0530

    issues

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var gulp = require('gulp');
 var glob = require('glob');
 var shelljs = require('shelljs');
 var path = require('path');
-  
+
 var components;
 var compPaths = glob.sync(`./src/**/`, { silent: true, ignore: [`./src/base/`, `./src/`, './src/common/', './src/getting-started/'] });
 
@@ -14,48 +14,60 @@ var user_mail = process.env.GIT_MAIL;
 var is_temp = process.env.IS_TEMP;
 /**
  * Source shipping to gitlap
- */  
+ */
 gulp.task('ship-to-gitlap', function (done) {
     console.log('---check----' + user_mail);
     console.log('---user---' + user);
-    
+
     shelljs.exec(`git config --global user.email "${user_mail}"`);
     shelljs.exec(`git config --global user.name "${user}"`);
-    
+
     var changes = shelljs.exec(`git diff --name-only HEAD^ HEAD`);
     console.log('--changes----' + changes);
-    
+
     var changedFileNames = changes.stdout.split('\n');
-    console.log('--changedFileNames----' + changedFileNames);    
-        var gitPath = 'https://' + user + ':' + token + `@gitlab.syncfusion.com/testgroup/install-docs`;
-        console.log('Clone has been started...!');
-        var clone = shelljs.exec('git clone ' + gitPath + ' -b ' + branch + ' ' + `../../../gitlapRepo/install-docs`, {
-            silent: false
-        });
-        if (clone.code !== 0) {
-            console.log(clone.stderr);
-            done(); 
-            return;
-        } else {
-            console.log('Clone has been completed...!');
-            // update src from github to gitlap - replace files from cloed repo
-			var rootDir = path.resolve('../../../gitlapRepo/install-docs');
-			var rootDir2 = path.resolve('../install-docs');
-			console.log('Directory...!' + rootDir);
-			console.log('Directory...!' + rootDir2);
-			for (var i = 0; i < changedFileNames.length; i++) 
-			{
-				 console.log('changes...!' +changedFileNames[i]);
-                shelljs.cp('-rf', `../install-docs/`+changedFileNames[i], `../../../gitlapRepo/install-docs/`+changedFileNames[i])
-            } 
-            shelljs.cd(`../../../gitlapRepo/install-docs`);
-			shelljs.exec('git rm --cached install-docs');
-            shelljs.exec('git add .');
-            shelljs.exec('git pull');
-            shelljs.exec('git commit -m \"source updation from github repo \" --no-verify');
-            shelljs.exec('git push');
-            shelljs.cd('../../')
+    console.log('--changedFileNames----' + changedFileNames);
+    var gitPath = 'https://' + user + ':' + token + `@gitlab.syncfusion.com/testgroup/install-docs`;
+    console.log('Clone has been started...!');
+    var clone = shelljs.exec('git clone ' + gitPath + ' -b ' + branch + ' ' + `../../../gitlapRepo/install-docs`, {
+        silent: false
+    });
+    if (clone.code !== 0) {
+        console.log(clone.stderr);
+        done();
+        return;
+    } else {
+        console.log('Clone has been completed...!');
+        // update src from github to gitlap - replace files from cloed repo
+        var rootDir = path.resolve('../../../gitlapRepo/install-docs');
+        var rootDir2 = path.resolve('../install-docs');
+        console.log('Directory...!' + rootDir);
+        console.log('Directory...!' + rootDir2);
+        for (var i = 0; i < changedFileNames.length; i++) {
+            console.log('changes...!' + changedFileNames[i]);
+            if (fs.existsSync('../install-docs/' + changedFileNames[i])) {
+                if (fs.existsSync('../../../gitlapRepo/install-docs/' + changedFileNames[i])) {
+                    shelljs.cp('-rf', `../install-docs/` + changedFileNames[i], `../../../gitlapRepo/install-docs/` + changedFileNames[i])
+                }
+                else {
+                    if (fs.existsSync('../../../gitlapRepo/install-docs/')) {
+                        shelljs.cp('-rf', `../install-docs/` + changedFileNames[i], `../../../gitlapRepo/install-docs/` + changedFileNames[i])
+                    }
+                }
+
+            }
+            else {
+                shelljs.rm('-rf', `../../../gitlapRepo/install-docs/` + changedFileNames[i])
+            }
         }
+        shelljs.cd(`../../../gitlapRepo/install-docs`);
+        shelljs.exec('git rm --cached install-docs');
+        shelljs.exec('git add .');
+        shelljs.exec('git pull');
+        shelljs.exec('git commit -m \"source updation from github repo \" --no-verify');
+        shelljs.exec('git push');
+        shelljs.cd('../../')
+    }
 })
 
 /**
@@ -77,7 +89,7 @@ gulp.task('lint', function (done) {
             console.log('\n*** Markdown Lint Succeeded ***\n');
             done();
         }
-    }); 
+    });
 });
 
 // Controls List

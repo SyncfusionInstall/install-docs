ed79be6a5eee91969d362ab17bd7a44ee6c6f935
christopher <christopher.hirudayasamy@syncfusion.com>
2020-10-27 10:30:41 +0530

    issues

--- gulpfile.js
+++ gulpfile.js
@@ -49,7 +49,7 @@ gulp.task('ship-to-gitlap', function (done) {
                 shelljs.cp('-rf', `../install-docs/`+changedFileNames[i], `../../../gitlapRepo/install-docs/`+changedFileNames[i])
             } 
             shelljs.cd(`../../../gitlapRepo/install-docs`);
-            shelljs.exec('git add .');
+            shelljs.exec('git add install-docs/');
             shelljs.exec('git pull');
             shelljs.exec('git commit -m \"source updation from github repo \" --no-verify');
             shelljs.exec('git push');
